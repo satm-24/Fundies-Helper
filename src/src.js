420fbac539f46b874e27a2d767e5a672acdac126
Kenny Jung <dukyoung01@naver.com>
2020-11-15 17:42:37 -0500

    add past exams and projects pages

--- src/src.js
+++ src/src.js
@@ -1,16 +1,13 @@
-// alert("src1")
-
 $(document).ready(function() {
     $.ajax({
         type: "GET",
-        // ../src/scraper/piazza_scrape.csv
         url: "../src/scraper/piazza_scrape.csv",
         dataType: "text",
-        success: function(data) {processData(data);}
+        success: function(data) {piazza(data);}
      });
 });
 
-function processData(allText) {
+function piazza(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split();
     var lines = [];
@@ -32,6 +29,37 @@ function processData(allText) {
     document.getElementsByClassName("piazza-three")[0].innerHTML = lines[2];
     document.getElementsByClassName("piazza-four")[0].innerHTML = lines[3];
     document.getElementsByClassName("piazza-five")[0].innerHTML = lines[4];
+}
 
-    // alert("src2")
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "../src/scraper/fundies_scrape.csv",
+        dataType: "text",
+        success: function(data) {hwLab(data);}
+     });
+});
+
+function hwLab(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split();
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split();
+
+        if (data.length == headers.length) {
+            var row = [];
+            for (var j=0; j<headers.length; j++) {
+                // row.push(headers[j]+":"+data[j]);
+                row.push(data[j]);
+            }
+            lines.push(row);
+        }
+    }
+    // document.getElementsByClassName("piazza-one")[0].innerHTML = lines[0];
+    // document.getElementsByClassName("piazza-two")[0].innerHTML = lines[1];
+    // document.getElementsByClassName("piazza-three")[0].innerHTML = lines[2];
+    // document.getElementsByClassName("piazza-four")[0].innerHTML = lines[3];
+    // document.getElementsByClassName("piazza-five")[0].innerHTML = lines[4];
 }
